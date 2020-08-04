cbc73f3688303c15caa5fb03a22a4d4ba149d059
computergraphicsforall <computergraphicsforall@gmail.com>
2020-08-04 14:42:25 -0500

    primera version

--- docs/menus.js
+++ docs/menus.js
@@ -659,9 +659,6 @@ function menuStaticChoropleth(id) {
                     addInfoChoropleticVisualization ('features');
                     addInteractiveInfoVisualization (ID_EX_CHOPLETH_PRED, ID_LOCALITIES_SOURCE, 'COD_LCL', 'constancy', DATA_ANA);
                     
-                    
-                    
-                    setLayoutProperty(ID_EX_CHOPLETH_PRED, 'visibility', 'visible');
                     item.innerText = item.innerText.toString().replace('Show', 'Hide');
 
                     let dataProperties = getPropertiesByData (DATA_ANA, 'COD_LCL');
@@ -670,7 +667,7 @@ function menuStaticChoropleth(id) {
                         addChoropleticProperties(dataProperties, ID_EX_CHOPLETH_PRED, ID_LOCALITIES_SOURCE, DATA_ANA, 'COD_LCL', CLICK_EVENT);     
                     }
                     addLayerChoroplethMap (ID_EX_CHOPLETH_PRED, ID_LOCALITIES_SOURCE, 'none', DATA_ANA, 'constancy', 'COD_LCL');
-
+                    setLayoutProperty(ID_EX_CHOPLETH_PRED, 'visibility', 'visible');
                 }
                 else {
                     removeLayer(ID_EX_CHOPLETH_PRED);
